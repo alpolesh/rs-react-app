e4f10eb9716f4f02c94e80994d7f3daac971a573
Andrey Poleshchuk <a.paleshchuk@brimit.com>
2025-07-25 23:51:47 +0300

    feat: add tests

---
+++ src/components/detailedView/DetailedView.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import DetailedView from './DetailedView';
+import type { Game } from '@src/types/game';
+
+const mockGame: Game = {
+  id: '123',
+  name: 'The Legend of Zelda',
+  released_date: '1986-02-21',
+  developer: 'Nintendo',
+  description: 'The first Zelda game.',
+  publisher: 'Nintendo',
+};
+
+describe('DetailedView', () => {
+  it('renders nothing if selectedGame is null', () => {
+    const { container } = render(
+      <DetailedView selectedGame={null} loadGameError={null} />
+    );
+    expect(container.firstChild).toBeNull();
+  });
+
+  it('renders error if loadGameError is provided', () => {
+    render(
+      <DetailedView selectedGame={mockGame} loadGameError="Failed to load" />
+    );
+    expect(screen.getByText(/Failed to load/i)).toBeInTheDocument();
+  });
+
+  it('renders game details if selectedGame is provided and no error', () => {
+    render(<DetailedView selectedGame={mockGame} loadGameError={null} />);
+
+    expect(screen.queryByText('Id')).not.toBeInTheDocument();
+    expect(screen.getByText('The Legend of Zelda')).toBeInTheDocument();
+    expect(screen.getByText('1986-02-21')).toBeInTheDocument();
+  });
+});
