78ac5e70af17906d96369bc5e4c0d1c5bbeba2d0
Andrey Poleshchuk <a.paleshchuk@brimit.com>
2025-08-29 01:02:48 +0300

    feat: add year selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import CountriesList from './components/CountriesList/CountriesList';
 import Spinner from './components/Spinner/Spinner';
+import YearInput from './components/YearInput/YearInput';
 
 export default function App() {
+  const [selectedYear, setSelectedYear] = useState<number | undefined>(
+    undefined
+  );
   return (
     <Suspense fallback={<Spinner />}>
-      <CountriesList />
+      <YearInput
+        selectedYear={selectedYear}
+        setSelectedYear={setSelectedYear}
+      />
+      <CountriesList selectedYear={selectedYear} />
     </Suspense>
   );
 }
--- src/components/CountriesList/CountriesList.tsx
+++ src/components/CountriesList/CountriesList.tsx
@@ -3,28 +3,42 @@ import { loadCo2Data } from '../../resource/co2Resource';
 interface RowData {
   name: string;
   iso_code: string;
+  populationLast: number | 'N/A';
+  year: number | 'N/A';
   population: number | 'N/A';
-  year: number;
   co2: number | 'N/A';
   co2_per_capita: number | 'N/A';
 }
-export default function CountriesList() {
+
+type Props = {
+  selectedYear: number | undefined;
+};
+export default function CountriesList({ selectedYear }: Props) {
   const data = loadCo2Data();
 
   const rows: RowData[] = Object.entries(data).map(([name, country]) => {
-    const latestData = [...country.data]
+    const latestPopulationData = [...country.data]
       .reverse()
       .find((d) => d.population !== undefined);
 
-    const y = latestData ?? country.data[country.data.length - 1];
+    const populationLast = latestPopulationData?.population ?? 'N/A';
+
+    const latestYearData = [...country.data]
+      .reverse()
+      .find((d) => d.year !== undefined);
+
+    const yearData = selectedYear
+      ? country.data.find((d) => d.year === selectedYear)
+      : latestYearData;
 
     return {
       name,
       iso_code: country.iso_code ?? 'N/A',
-      population: y.population ?? 'N/A',
-      year: y.year,
-      co2: y.co2 ?? 'N/A',
-      co2_per_capita: y.co2_per_capita ?? 'N/A',
+      populationLast,
+      year: yearData?.year ?? 'N/A',
+      population: yearData?.population ?? 'N/A',
+      co2: yearData?.co2 ?? 'N/A',
+      co2_per_capita: yearData?.co2_per_capita ?? 'N/A',
     };
   });
 
@@ -36,6 +50,7 @@ export default function CountriesList() {
           <th className="border border-gray-300 px-2 py-1">Population</th>
           <th className="border border-gray-300 px-2 py-1">ISO Code</th>
           <th className="border border-gray-300 px-2 py-1">Year</th>
+          <th className="border border-gray-300 px-2 py-1">Population</th>
           <th className="border border-gray-300 px-2 py-1">CO2</th>
           <th className="border border-gray-300 px-2 py-1">CO2 per capita</th>
         </tr>
@@ -45,10 +60,13 @@ export default function CountriesList() {
           <tr key={row.name}>
             <td className="border border-gray-300 px-2 py-1">{row.name}</td>
             <td className="border border-gray-300 px-2 py-1">
-              {row.population}
+              {row.populationLast}
             </td>
             <td className="border border-gray-300 px-2 py-1">{row.iso_code}</td>
             <td className="border border-gray-300 px-2 py-1">{row.year}</td>
+            <td className="border border-gray-300 px-2 py-1">
+              {row.population}
+            </td>
             <td className="border border-gray-300 px-2 py-1">{row.co2}</td>
             <td className="border border-gray-300 px-2 py-1">
               {row.co2_per_capita}
